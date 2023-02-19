1f3fa317087e2420e9fcd1a9ea850ccb52129729
mahabubsaki <md929820@gmail.com>
2023-02-19 15:25:13 +0600

    components created and styles added

---
+++ components/HeroBanner.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import React from 'react';
+
+const HeroBanner = () => {
+    return (
+        <div className='hero-banner-container'>
+            <div>
+                <p className='beats-solo'>SMALL TEXT</p>
+                <h3>Mid Text</h3>
+                <img src="" alt="headphones" className='hero-banner-image' />
+                <div>
+                    <Link href={'/products/ID'}>
+                        <button type='button'>BUTTON TEXT</button>
+                    </Link>
+                    <div className='desc'>
+                        <h5>Description</h5>
+                        <p>DESCRIPTION</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeroBanner;
